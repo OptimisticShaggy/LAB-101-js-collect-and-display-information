db1b0dcb59c08dc18655f80a84149d848c572bc8
OptimisticShaggy <58822932+OptimisticShaggy@users.noreply.github.com>
2020-11-10 19:30:43 -0500

    Warnings corrected

--- js/script.js
+++ js/script.js
@@ -1,39 +1,31 @@
-var vfName = document.getElementById('fName')
-var vlName = document.getElementById('lName')
-var vdText = document.getElementById('dText')
-var vuEmail = document.getElementById('uEmail')
-var vuPhone = document.getElementById('uPhone')
+let vfName = document.getElementById('fName')
+let vlName = document.getElementById('lName')
+let vdText = document.getElementById('dText')
+let vuEmail = document.getElementById('uEmail')
+let vuPhone = document.getElementById('uPhone')
+let textINNERHTML = document.getElementById('textINNERHTML')
+let textHTML = document.getElementById('textHTML')
 
-/// The lines above store all the data from the elements with the specific ID inside the variable to the left
+/// The lines above store all the data from the elements with the specific ID inside the letiable to the left
 
-var updateForm = function() {
-  var vvfName = vfName.value;
-  var vvlName = vlName.value;
-  var vvdText = vdText.value;
-  var vvuEmail = vuEmail.value;
-  var vvuPhone = vuPhone.value;
+let updateForm = function () {
+  let vvfName = vfName.value
+  let vvlName = vlName.value
+  let vvdText = vdText.value
+  let vvuEmail = vuEmail.value
+  let vvuPhone = vuPhone.value
 
-/// Stores the value of the IDed element from the top of the list into the variable to the left.
+  /// Stores the value of the IDed element from the top of the list into the letiable to the left.
 
-textINNERHTML.innerHTML =
-'<h1>Hi, my name is ' + vvfName + vvlName + '!</h1>\
-<p> ' + vvdText + ' </p>\
-<p>\
-If you\'re interested in a date, you can email me at <a href="mailto:' + vvuEmail + '" target="_blank">' + vvuEmail + '</a> or give me a call at <a href="tel:' + vuPhone + '" target="_blank">' + vvuPhone + '</a>.\
-  </p>\
-';
+  textINNERHTML.innerHTML =
+'<h1>Hi, my name is ' + vvfName + vvlName + '!</h1><p> ' + vvdText + ' </p><p>If you\'re interested in a date, you can email me at <a href="mailto:' + vvuEmail + '" target="_blank">' + vvuEmail + '</a> or give me a call at <a href="tel:' + vuPhone + '" target="_blank">' + vvuPhone + '</a>.</p>'
 
-/// Passes all the text and variable's value from the list above and injects this text directly in the HTML document.
+  /// Passes all the text and letiable's value from the list above and injects this text directly in the HTML document.
 
-textHTML.textContent =
-'<h1>Hi, my name is ' + vvfName + vvlName + '!</h1>\
-<p> ' + vvdText + ' </p>\
-<p>\
-If you\'re interested in a date, you can email me at <a href="mailto:' + vvuEmail + '" target="_blank">' + vvuEmail + '</a> or give me a call at <a href="tel:' + vvuPhone + '" target="_blank">' + vvuPhone + '</a>.\
-  </p>\
-';
+  textHTML.textContent =
+'<h1>Hi, my name is ' + vvfName + vvlName + '!</h1><p> ' + vvdText + ' </p><p>If you\'re interested in a date, you can email me at <a href="mailto:' + vvuEmail + '" target="_blank">' + vvuEmail + '</a> or give me a call at <a href="tel:' + vvuPhone + '" target="_blank">' + vvuPhone + '</a>.</p>'
 
-/// Passes all the text and variable's value from the list above and injects this HTML directly in the HTML document.
+  /// Passes all the text and letiable's value from the list above and injects this HTML directly in the HTML document.
 }
 
 vfName.addEventListener('input', updateForm)
